6ffcdc9e3a9f7b8f4daa6c9e9be2d77e32a96ecd
Yeroman1 <101729726+Yeroman1@users.noreply.github.com>
2026-04-01 14:45:55 +0300

    Create Partition Label.py

---
+++ Two Pointers/Partition Label.py
@@ -0,0 +1,11 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last = {c:i for i,c in enumerate(s)}
+        a = []
+        l = r = 0
+        for i, c in enumerate(s):
+            r = max(r, last[c])
+            if i == r:
+                a.append(r - l + 1)
+                l = i + 1
+        return a
